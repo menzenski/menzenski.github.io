a55427512972f3798676ec500c9d80f622ce38d3
Matt Menzenski <matt@mobilgov.com>
2019-08-10 18:20:10 -0500

    add and respect gitignore

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Welcome",
-        "excerpt":"Welcome to my rebuilt blog!  ","categories": ["blog"],
-        "tags": ["welcome"],
-        "url": "http://localhost:4000/blog/welcome/",
-        "teaser":null}]
